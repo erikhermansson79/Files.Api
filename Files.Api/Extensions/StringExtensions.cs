5f1cfebfd1224dbc539246e195d30950a07fc67c
Erik Hermansson <erik.hermansson@swedwise.se>
2024-10-15 10:21:01 +0200

    Added filtering of reserved characters.

---
+++ Files.Api/Extensions/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Files.Api.Extensions
+{
+	internal static class StringExtensions
+	{
+		[return: NotNullIfNotNull(nameof(value))]
+		public static string? ReplaceReservedCharacters(this string? value)
+		{
+			if (value == null)
+			{
+				return null!;
+			}
+
+			return value.Replace('+', '_').Replace("&", "_").Replace('%', '_');
+		}
+	}
+}
